d287784152dfbcee8d6656d7d98c1a1e85cf8cc6
David Camargo <davidcp90@gmail.com>
2015-12-09 15:58:48 -0500

    static root

--- bitgray/settings.py
+++ bitgray/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/home/django/bitgray/static/'
 
 #SUPER USER
 #bitgray
