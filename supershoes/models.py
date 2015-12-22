05db1b98ba7b6e278864734b9f18d493820382a3
David Camargo <davidcp90@gmail.com>
2015-12-22 13:39:34 -0500

    models and migrations

--- supershoes/models.py
+++ supershoes/models.py
@@ -2,4 +2,32 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Stores(models.Model):
+
+    """
+        model for stores
+    """
+    name = models.TextField(verbose_name=u'Store Name')
+    address = models.CharField(max_length=200, null=True, blank=True,
+                               verbose_name=u'Store Address')
+
+
+class Articles(models.Model):
+
+    """
+        model for articles
+    """
+    name = models.TextField(verbose_name=u'Article Name')
+    description = models.TextField(
+        verbose_name=u'Article Description', blank=True, null=True)
+    price = models.IntegerField(default=0, verbose_name=u'Article Price')
+    total_in_shelf = models.IntegerField(
+        default=0, verbose_name=u'Total Articles in Shelf')
+    total_in_vault = models.IntegerField(
+        default=0, verbose_name=u'Total Articles in Vault')
+    store = models.ForeignKey(
+        'supershoes.Stores',
+        related_name='articles_stores',
+        verbose_name='Store'
+    )
